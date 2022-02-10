cde22a8e7bfceaae1704b131ceb01e028d250770
kurkovsky <stankurkovsky@gmail.com>
2022-02-10 16:38:01 +0000

    added a multithreaded C++ code example

---
+++ 04-CPP/threads.cpp
@@ -0,0 +1,57 @@
+// a simple multi-threaded program
+// compile: g++ threads.cpp -lpthread
+
+#include <iostream>
+#include <pthread.h>
+#include <string.h>
+using namespace std;
+
+#define NUM_THREADS 5
+
+void *PrintHello(void *threadid){
+    long tid;
+    tid = (long)threadid;
+    cout << "Hello world! Thread ID " << tid << endl;
+    pthread_exit(NULL); 
+}
+
+struct thread_data { // used to illustrate how to pass larger amounts of data into a thread
+    int thread_id;
+    char message[100];
+};
+
+void *PrintHello2(void *threadarg){
+    struct thread_data *my_data;
+    my_data = (struct thread_data *)threadarg;
+
+    cout << "Thread ID : " << my_data->thread_id << endl;
+    cout << "Message : " << my_data->message << endl;
+    pthread_exit(NULL); 
+}
+
+
+int main(){
+    pthread_t threads[NUM_THREADS];
+    struct thread_data td[NUM_THREADS];
+    int rc;
+
+    for(long i = 0; i < NUM_THREADS; i++){
+        cout << "main(): creating thread, " << i << endl;
+
+        // a simple example when we pass only a single number into a thread
+        // rc = pthread_create(&threads[i], NULL, PrintHello, (void *)i);
+
+        td[i].thread_id = i;
+        sprintf(td[i].message, "This is message # %ld", i);
+
+        // a more complex example when we pass a record into a thread
+
+        rc=pthread_create(&threads[i], NULL, PrintHello2, (void*)&td[i]);
+        if(rc) {
+            cout << "Error: unable to create thread, " << rc << endl;
+            exit(-1);
+        }
+    }
+    pthread_exit(NULL); 
+    return 0;
+}
